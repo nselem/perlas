4fd5191917b780b531ab1ad5a85a3dbf634da9b9
nselem35 <nselem35@hotmail.com>
2014-11-06 14:44:13 -0600

    I have added a read file to ENSAMBLE

--- ENSAMBLADO/velvet.pl
+++ ENSAMBLADO/velvet.pl
@@ -6,12 +6,13 @@
 cd  $PBS_O_WORKDIR
 
 DIR=ENSAMBLADO
+## Kmer de tamanio 10 a 30 contando de 5 en 5 (10,15,20,...,30)
 for i in {10..30..5}
 do
 
-echo "/data/storage/software/velvet_1.2.10/velveth ${i} ${i} -fastq -shortPaired -separate $DIR/JP2A_1_p.fastq $DIR/JP2A_2_p.fastq -shortPaired2 -separate $DIR/JP2B_1_p.fastq JP2B_2_p.fastq -shortPaired3 -separate $DIR/JP2C_1_p.fastq $DIR/JP2C_2_p.fastq > velh.log"
+/data/storage/software/velvet_1.2.10/velveth ${i} ${i} -fastq -shortPaired -separate $DIR/JP2A_1_p.fastq $DIR/JP2A_2_p.fastq -shortPaired2 -separate $DIR/JP2B_1_p.fastq JP2B_2_p.fastq -shortPaired3 -separate $DIR/JP2C_1_p.fastq $DIR/JP2C_2_p.fastq > velh.log
 
-echo "/data/storage/software/velvet_1.2.10/velvetg ${i} -ins_length 687  -ins_length_sd 350 -ins_length2 687  -ins_length_sd2 350 -ins_length3 687  -ins_length_sd3 350  -min_contig_lgth 1000 -scaffolding yes -exp_cov auto -cov_cutoff 5 > Velg.log"
+/data/storage/software/velvet_1.2.10/velvetg ${i} -ins_length 687  -ins_length_sd 350 -ins_length2 687  -ins_length_sd2 350 -ins_length3 687  -ins_length_sd3 350  -min_contig_lgth 1000 -scaffolding yes -exp_cov auto -cov_cutoff 5 > Velg.log
 
 done
 
