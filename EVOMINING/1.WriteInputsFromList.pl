8be73a73f07a5ee14802d94673ba0b977e5e9f6f
nselem35 <nselem35@hotmail.com>
2015-10-21 17:04:10 -0500

    I have modified Evomining context to draw all in one svg and to recibe ids from terminal not from a file

--- EVOMINING/1.WriteInputsFromList.pl
+++ EVOMINING/1.WriteInputsFromList.pl
@@ -1,34 +1,49 @@
 use strict;
 use warnings;
 
-my $file=$ARGV[0]; ##File with genome ids
-my $fileNames=$ARGV[1]; ##File with genome ids
+
+########3 Nota neceisto corregir que lea la cantidad total de genes del archivo total de base de datos de genomas.
+## This script generates one images with all the ids on file
+
+`rm *.input`;
+#print "Number of contexts $NumberContexts\n";
+my $fileNames=shift; ##File with genome ids RASt.ids
+my @gen_id=@ARGV; ##File with genome ids to get the contexts
 
 #print "Reading $file\n";
 my %LIST;
 my %NAMES;
-my $ClusterSize=3; ##Gen number around
+my $long=80000;
+my $ClusterSize=100; ##Gen number around
+
+print "$fileNames\n";
+print "$ARGV[0],$ARGV[1]!\n";
 
 ###################### Main ########################################
-read_list($file,$fileNames,\%LIST,\%NAMES);
-
-foreach my $gi (keys %LIST){
-	my $gi_file=$LIST{$gi};
-	#print "working on $gi on $gi_file\n";
-	ContextArray($gi_file,$gi,\%NAMES);
-}
-
+	read_list($fileNames,\%LIST,\%NAMES,@gen_id);
+	foreach my $gi (keys %LIST){
+		my $gi_file=$LIST{$gi};
+		#print "working on $gi on $gi_file\n";
+		ContextArray($gi_file,$gi,$long,\%NAMES);
+		}
+######################################################################
+###################### Subs ##########################################
 
 #____________________________________________________________________________________________
 sub ContextArray{
 	my $orgs=shift;
 	my $peg=shift;
+	my $long=shift;
 	my $refNAMES=shift;	
 
 	open(FILE,">$peg.input")or die "could not open $orgs.input file $!";
 	my @CONTEXT;
 
 	my ($hit0,$start0,$stop0,$dir0,$func0,$contig0)=getInfo($peg,$orgs);
+	my $midle0=int(($start0+$stop0)/2);
+	#my $sizeFile=`grep '>' $orgs.faa | wc`;
+	#print "Total $sizeFile";
+
 	$CONTEXT[0]=[$hit0,$start0,$stop0,$dir0,$func0];
 	print FILE "$CONTEXT[0][1]\t$CONTEXT[0][2]\t$CONTEXT[0][3]\t1\t$refNAMES->{$peg}\t$CONTEXT[0][4]\t$CONTEXT[0][0]\n";
 	my $count=1;	
@@ -38,20 +53,19 @@ sub ContextArray{
 	for (my $i=$genId-$ClusterSize;$i<$genId+$ClusterSize;$i++){
 		my $neighbourId=$sp[0].".".$sp[1].".".$i;
 		my ($hit,$start,$stop,$dir,$func,$contig)=getInfo($neighbourId,$orgs);
-		my $color;
-				
-		if(!($hit eq "")){
-			if($contig0 eq $contig){
-				$CONTEXT[$count]=[$hit,$start,$stop,$dir,$func];
+		if(!($hit eq "")){				
+			my $midle=int(($start+$stop)/2);
+			my $dist=abs($midle-$midle0);
+			if($dist<$long){
+				if($contig0 eq $contig){
+					$CONTEXT[$count]=[$hit,$start,$stop,$dir,$func];
+					}
 				}
-			}
-		$color=0;			
-#		print "$peg, $org, $color \n";
-
-		if ($contig0 eq $contig and $i!=$genId){
-  			#print "$contig0 $contig\n";
-			print FILE "$CONTEXT[$count][1]\t$CONTEXT[$count][2]\t$CONTEXT[$count][3]\t0\t$refNAMES->{$peg}\t$CONTEXT[$count][4]\t$CONTEXT[$count][0]\n";		
-			$count++;
+			if ($contig0 eq $contig and $i!=$genId){
+  				#print "$contig0 $contig\n";
+				print FILE "$CONTEXT[$count][1]\t$CONTEXT[$count][2]\t$CONTEXT[$count][3]\t0\t$refNAMES->{$peg}\t$CONTEXT[$count][4]\t$CONTEXT[$count][0]\n";		
+				$count++;
+				}
 			}
 		}
 close FILE;
@@ -71,12 +85,20 @@ sub getInfo{
 	
 	my $Grep=`grep 'peg.$sp0[2]\t' $file.txt`;
 	my @sp=split("\t",$Grep);
-	my $contig=$sp[0];	
-	my $hit=$sp[1];
-	my $start=$sp[4];
-	my $stop=$sp[5];
-	my $dir=$sp[6];
-	my $func=$sp[7];
+	my $contig="";	
+	my $hit="";
+	my $start="";
+	my $stop="";
+	my $dir="";
+	my $func="";
+	if($sp[7]){
+ 		$contig=$sp[0];	
+		$hit=$sp[1];
+		$start=$sp[4];
+		$stop=$sp[5];
+		$dir=$sp[6];
+		$func=$sp[7];
+		}
 	#print "hit $hit start $start stop $stop dir $dir func $func cont $contig\n\n";	
 	return ($hit,$start,$stop,$dir,$func,$contig);
 }
@@ -84,19 +106,20 @@ sub getInfo{
 
 #_________________________________________________________________________________
 sub read_list{  ## Get the files where the genes belongs
-	my $file=shift;
+
 	my $fileNames=shift;	
 	my $refLIST=shift;
 	my $refNAMES=shift;
-	open (FILE, $file) or die "Could not open file $file \n $!";
+	my @ID=@_;
+	#open (FILE, $file) or die "Could not open file $file \n $!";
 
-	for my $line (<FILE>){
-		chomp $line;
+	for my $gen_id (@ID){
+	#	chomp $line;
 		#print "$line\n";
-		my @sp=split(/\./,$line);
+		my @sp=split(/\./,$gen_id);
 
 		if ($sp[0] and $sp[1]){
-#			print("0:$sp[0],1:$sp[1],2:$sp[2]\n");
+			print("0:$sp[0],1:$sp[1],2:$sp[2]\n");
 			my $genome=$sp[0].".".$sp[1];
 			open (NAMES,$fileNames) or die "Could not open names file\n $!";
 				for my $line2 (<NAMES>){
@@ -104,13 +127,13 @@ sub read_list{  ## Get the files where the genes belongs
 					my @sp2=split(/\t/,$line2);
 					if($line2=~/$genome/){
 						#print "$line\t$sp2[0]\t$sp2[2]\n";
-						$refLIST->{$line}=$sp2[0];
-						$refNAMES->{$line}=$sp2[2];
+						$refLIST->{$gen_id}=$sp2[0];
+						$refNAMES->{$gen_id}=$sp2[2];
 						last;
 					 	}
 					close NAMES;
 					}						
 				}
 			}
-	close FILE;	
+	#close FILE;	
 	}
